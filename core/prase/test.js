7c305e33ac34782519ffdbd610e2fcc14fdff199
蔡春保 <chunbao.cai@17zuoye.com>
2021-04-22 20:49:49 +0800

    .

---
+++ core/prase/test.js
@@ -0,0 +1,13 @@
+/**
+ * Author: chunbao.cai
+ * Description: simple template -> ast
+ *
+*/
+
+const template = `
+    <div>
+        <div style="color: red">{{ count }}</div>
+        <button @click="addCount">addCount</button>
+    </div>
+`
+
